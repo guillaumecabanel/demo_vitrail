c73cfb1bf6c322b5a1e1534d8725bf4037241d40
Guillaume CABANEL <guillaume@kuartz.fr>
2024-04-22 13:52:32 +0200

    vitrail usage

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def show
+    @users ||= [
+      OpenStruct.new(id: 1, name: "Alice", email: "alice@example.com"),
+      OpenStruct.new(id: 2, name: "Bob", email: "bob@example.com"),
+      OpenStruct.new(id: 3, name: "Charlie", email: "charlie@example.com"),
+    ]
+  end
+end
